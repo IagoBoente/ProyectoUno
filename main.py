ccf1170c09431b7640f7783c27c11ee05f2d7ee7
a19iagobf <iagoboente03@gmail.com>
2020-10-29 08:54:36 +0100

    V0.0.2 Añadiendo radiobutton y checked

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ventana import *
-import sys, var, events
+import sys, var, events, clients
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
@@ -7,11 +7,20 @@ class Main(QtWidgets.QMainWindow):
         var.ui = Ui_venPrincipal
         var.ui.setipUi(self)
 
+        '''
+        colección de datos
+        '''
+        var.rbtSexo = (var.ui.rbtFem, var.ui.rbtMasc)
+
     '''
     conexion de eventos con los objetos
+    estamos conectando el codigo con la interfaz grafica
     '''
     var.ui.btnSalir.clicked.connect(events.Eventos.Salir)
     var.ui.actionSalir.triggered.connect(events.Eventos.Salir)
+    var.ui.editDni.editingFinished.connect(clients.Clientes.validoDni)
+    for i in var.rbtSexo:
+        i.toggled.connect(clients.Clientes.selSexo)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
